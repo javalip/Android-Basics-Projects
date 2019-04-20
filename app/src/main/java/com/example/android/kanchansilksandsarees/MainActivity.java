3c1b73ba207132f3687b9d7fde8dcb6c5829762f
pjavali <pjavali@ebay.com>
2019-04-19 17:20:36 -0700

    Project1

---
+++ app/src/main/java/com/example/android/kanchansilksandsarees/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.kanchansilksandsarees;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/***
+ * Business image from google
+ * phone, hourglass and location icons from material design
+ *color codrs material design
+ * coding lesson from udacity;)
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
